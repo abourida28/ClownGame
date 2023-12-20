2c020b5ff3996211d62beb77bf44affe6e460861
MohabG2 <mohabgobran@gmail.com>
2023-12-21 00:45:08 +0200

    Score and timeout

--- src/Backend/Objects/Shape.java
+++ src/Backend/Objects/Shape.java
@@ -4,7 +4,7 @@ import eg.edu.alexu.csd.oop.game.GameObject;
 import eg.edu.alexu.csd.oop.game.World;
 
 public interface Shape extends GameObject {
-    void fall(World balloonWorld);
+    void fall(World Worldimpl);
     int getFallingSpeed();
     //Shape createShape();
     void setFallingSpeed(int speed);
--- src/Backend/Worldimpl.java
+++ src/Backend/Worldimpl.java
@@ -1,5 +1,7 @@
 package Backend;
 
+import Backend.Objects.Shape;
+import FOR_TESTING.DifficultyManager;
 import eg.edu.alexu.csd.oop.game.World;
 
 import java.awt.*;
@@ -23,58 +25,66 @@ public class Worldimpl implements World {
         width = screenWidth;
         height = screenHeight;
         for(int i=0; i<2; i++) {
-            //Mesh fahem eh el 90 dy
-            control.add(new Clown(screenWidth*5/11, screenHeight*i/8,  true, Color.BLUE));
+            control.add(new Clown(width*5/11, height*i/8,  true, Color.BLUE));
         }
         for(int i=0;i<10;i++) {
-            //dh new Shape() interface
-//            moving.add(new Shape());
+            RandomShapeGenerator randomShapeGenerator = new RandomShapeGenerator(DifficultyManager.getEasyDifficultyFactories());
+            int posX = (int) (Math.random() * width);
+            int posY = -1 * (int) (Math.random() * height);
+            Shape randomShape = randomShapeGenerator.createRandomShape(posX, posY,1);
+            moving.add(randomShape);
         }
 
+    }
+    private boolean intersect(Shape s1, Clown s2){
+        return (Math.abs((s1.getX()+s1.getWidth()/2) - (s2.getX()+s2.getWidth()/2)) <= s1.getWidth()) && (Math.abs((s1.getY()+s1.getHeight()/2) - (s2.getY()+s2.getHeight()/2)) <= s1.getHeight());
+    }
+    @Override
+    public boolean refresh()
+    {
+        boolean timeout = System.currentTimeMillis() - startTime > MAX_Time;
+        Clown Clown = (Backend.Objects.Clown) control.get(0);
+        for (GameObject fallObj : moving) {
+            Shape shape = (Shape) fallObj;
+            shape.fall(this);
+            if(!timeout && intersect((Shape) fallObj, Clown)){
+                score = Math.max(0, score+1);
+    }
+        }
+        return !timeout;
+    }
+    @Override
+    public int getSpeed() {
+        return 7;
+    }
+    @Override
+    public int getControlSpeed() {
+        return 7;
     }
 
     @Override
     public List<GameObject> getConstantObjects() {
         return null;
     }
-
     @Override
     public List<GameObject> getMovableObjects() {
-        return null;
+        return moving;
     }
-
     @Override
     public List<GameObject> getControlableObjects() {
-        return null;
+        return control;
     }
-
     @Override
     public int getWidth() {
-        return 0;
+        return width;
     }
-
     @Override
     public int getHeight() {
-        return 0;
+        return height;
     }
-
-    @Override
-    public boolean refresh() {
-        return false;
-    }
-
     @Override
     public String getStatus() {
-        return null;
+        return "Score=" + score + "   |   Time=" + Math.max(0, (MAX_Time - (System.currentTimeMillis()-startTime))/1000);
     }
-
-    @Override
-    public int getSpeed() {
-        return 0;
-    }
-
-    @Override
-    public int getControlSpeed() {
-        return 0;
-    }
+}
 }
